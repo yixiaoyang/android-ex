41366f0957b8c25fff6599cb8333a444135fedbc
xiaoyang <hityixiaoyang@gmail.com>
2012-02-17 15:02:49 +0800

    @xiaoyang add buttom menus

--- iFileManager/gen/cn/edu/hit/R.java
+++ iFileManager/gen/cn/edu/hit/R.java
@@ -11,24 +11,35 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int cut_selected=0x7f020000;
-        public static final int cut_unselected=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int lock_selected=0x7f020003;
-        public static final int lock_unselected=0x7f020004;
-        public static final int marker_selected=0x7f020005;
-        public static final int marker_unselected=0x7f020006;
-        public static final int splash1=0x7f020007;
-        public static final int splash2=0x7f020008;
-        public static final int splash3=0x7f020009;
-        public static final int splash4=0x7f02000a;
+        public static final int baraza_48=0x7f020000;
+        public static final int cut_selected=0x7f020001;
+        public static final int cut_unselected=0x7f020002;
+        public static final int exit_48=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int lock_selected=0x7f020005;
+        public static final int lock_unselected=0x7f020006;
+        public static final int marker_selected=0x7f020007;
+        public static final int marker_unselected=0x7f020008;
+        public static final int new_board_48=0x7f020009;
+        public static final int settings_48=0x7f02000a;
+        public static final int splash1=0x7f02000b;
+        public static final int splash2=0x7f02000c;
+        public static final int splash3=0x7f02000d;
+        public static final int splash4=0x7f02000e;
     }
     public static final class id {
-        public static final int imageView1=0x7f050000;
+        public static final int imageView1=0x7f060000;
+        public static final int itemAbout=0x7f060003;
+        public static final int itemExit=0x7f060004;
+        public static final int itemNew=0x7f060001;
+        public static final int itemsetting=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int splash=0x7f030001;
+    }
+    public static final class menu {
+        public static final int menu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- iFileManager/src/cn/edu/hit/IFileManagerActivity.java
+++ iFileManager/src/cn/edu/hit/IFileManagerActivity.java
@@ -1,10 +1,48 @@
 package cn.edu.hit;
 
+/*
+ * auther:xiaoyang 2012-2@HIT
+ * 
+ */
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class IFileManagerActivity extends Activity {
-    /** Called when the activity is first created. */
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu);
+		/*load buttom menu*/
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+    }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		 super.onOptionsItemSelected(item);
+		switch(item.getItemId()){
+		case R.id.itemNew:{
+			break;
+		}
+		case R.id.itemsetting:{
+			break;
+		}
+		case R.id.itemAbout:{
+			break;
+		}
+		case R.id.itemExit:{
+			break;
+		}
+		}
+		return true;
+	}
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
